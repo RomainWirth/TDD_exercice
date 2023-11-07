7c2d265ddde75e99506a8b0115e8118fb8a60ec2
Romain Wirth <wirth.romain@gmail.com>
2023-11-07 14:40:35 +0100

    UPD script.test.js add tests on sortTasks, removeTask, markTaskCompleted

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 export function addTask(object, array) {
     return array;
 };
+
+export function removeTask(index, array) {
+
+};
+
+export function sortTasks(array, indexOne, indexTwo) {
+
+};
+
+export function markTaskCompleted(array, object) {
+
+};
+
--- script.test.js
+++ script.test.js
@@ -1,7 +1,7 @@
 import { test, describe, expect, it, vi } from 'vitest'
 import {
     addTask, 
-    checkTaskObject, 
+    // checkTaskObject, 
     removeTask, 
     sortTasks, 
     markTaskCompleted, 
@@ -36,11 +36,71 @@ describe('addTask()', () => {
         expect(result[0]).toEqual(mockTask);
     })
 
-    mockTask = {};
-    result = addTask(mockTask, taskArray);
+    const invalidLabel = 123
     it('ADT02', () => {
-        expect(result).toHaveLength(1);
-        expect(result[0]).toEqual(mockTask);
+        const errorCallback = () => addTask(invalidLabel, taskArray)
+
+        expect(errorCallback).toThrow('Label must be a string')
     })
+
+    it('ADT03', () => {
+        const emptyTask = {};
+        addTask(taskArray, emptyTask);
+    
+        expect(taskArray).toHaveLength(0);
+     })
+})
+
+describe('removeTask()', () => {
+    const taskArray = [
+        { label: "task 1", done: false },
+        { label: "task 2", done: false },
+        { label: 'task 3', done: false },
+    ];
+    const indexToRemove = 1;
+    const removedTask = removeTask(indexToRemove, taskArray);
+    it('RMT01', () => {
+        expect(removedTask).toEqual({ label: 'task 2', done: false });
+        expect(taskArray).toEqual([
+            { label: "task 1", done: false },
+            { label: 'task 3', done: false }
+        ])
+    })
+})
+
+describe('sortTasks()', () => {
+    const taskArray = [
+        { label: "task 1", done: false },
+        { label: "task 2", done: false },
+        { label: "task 3", done: false }
+    ];
+    const indexOne = 0;
+    const indexTwo = 2;
+    
+    sortTasks(taskArray, indexOne, indexTwo);
+    
+    it('SRT01', () => {
+        expect(taskArray[indexOne]).toEqual({ label: 'task 3', done: false });
+        expect(taskArray[indexTwo]).toEqual({ label: 'task 1', done: false });
+    })
+})
+
+describe('markTaskCompleted()', () => {
+    it('MTC01', () => {
+        const taskArray = [
+          { label: 'Task 1', done: false },
+          { label: 'Task 2', done: false },
+          { label: 'Task 3', done: false },
+        ];
+    
+        const taskToMarkDone = taskArray[1];
+        const result = markTaskCompleted(taskArray, taskToMarkDone);
+    
+        expect(result).toEqual([
+          { label: 'Task 1', done: false },
+          { label: 'Task 2', done: true },
+          { label: 'Task 3', done: false },
+        ]);
+     });
 })
 
